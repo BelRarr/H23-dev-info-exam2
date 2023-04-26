d9a21b9fa3aa6d63b66d14caee4438767d804388
BeauBonhomme <robertpoirier@cogitoideation.com>
2023-04-26 16:45:01 -0400

    Archivage du code révisé Program.cs

--- Program.cs
+++ Program.cs
@@ -10,6 +10,11 @@ namespace H23_Dev_Info_Examen2
         {
             var liste = new List<int>();
             var moyenne = OperationsMathematiques.CalculerMoyenne(liste);
+
+            if(moyenne == -1)
+            {
+                Console.WriteLine("données invalides");
+            }
             Console.WriteLine(moyenne);
         }
     }
