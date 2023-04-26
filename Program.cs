2b9305908fa49fc8c09d264551425959fecd1f22
Bah <gadhatto1995@gmail.com>
2023-04-26 14:25:51 -0400

    Commit sur la modification de la methode Main

--- Program.cs
+++ Program.cs
@@ -7,10 +7,26 @@ namespace H23_Dev_Info_Examen2
     public class Program
     {
         public static void Main(string[] args)
-        {
-            var liste = new List<int>();
-            var moyenne = OperationsMathematiques.CalculerMoyenne(liste);
-            Console.WriteLine(moyenne);
-        }
+{
+    // Créer une nouvelle liste d'entiers vide.
+    var liste = new List<int>();
+
+    // Appeler la méthode statique CalculerMoyenne de la classe OperationsMathematiques
+    // en lui passant la liste vide créée précédemment comme argument.
+    var moyenne = OperationsMathematiques.CalculerMoyenne(liste);
+
+    // Vérifier si la moyenne est égale à -1.
+    if (moyenne == -1)
+    {
+        // Afficher un message d'erreur si la moyenne est égale à -1.
+        Console.WriteLine("Données invalides");
+    }
+    else
+    {
+        // Sinon, afficher la moyenne calculée à l'écran.
+        Console.WriteLine(moyenne);
+    }
+}
+
     }
 }
