239943a26466fff77808af41c03dbde7f8f97b93
BOUTITI H23 Dev Info <2294664@cegeplimoilou.ca>
2023-04-26 23:24:43 -0400

    Archivage Program.cs

--- Program.cs
+++ Program.cs
@@ -4,12 +4,18 @@ using System.Collections.Generic;
 
 namespace H23_Dev_Info_Examen2
 {
-    public class Program
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
+        var liste = new List<int>();
+        var moyenne = OperationsMathematiques.CalculerMoyenne(liste);
+
+        // Vérifier si la moyenne est égale à -1
+        if (moyenne == -1)
         {
-            var liste = new List<int>();
-            var moyenne = OperationsMathematiques.CalculerMoyenne(liste);
+            Console.WriteLine("données invalides");
+        }
+        else
+        {
             Console.WriteLine(moyenne);
         }
     }
